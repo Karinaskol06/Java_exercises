4b272aefe4b3a9b9cba2e35c7ecd10752aba8f8c
Karina <karinaskol06@gmail.com>
2025-06-27 13:41:13 +0300

    The second commit

--- src/main/java/SimpleSyntax/FileForGit.java
+++ src/main/java/SimpleSyntax/FileForGit.java
@@ -3,5 +3,6 @@ package SimpleSyntax;
 public class FileForGit {
     public static void main(String[] args) {
         System.out.println("This file will be added to other-test-branch by git");
+        System.out.println("The second commit");
     }
 }
