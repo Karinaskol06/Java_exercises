9ee1e94860e877eb8d380d1e783c3c4ec18facc4
Karina <karinaskol06@gmail.com>
2025-06-27 12:38:19 +0300

    Add a new file to the branch

---
+++ src/main/java/SimpleSyntax/FileForGit.java
@@ -0,0 +1,7 @@
+package SimpleSyntax;
+
+public class FileForGit {
+    public static void main(String[] args) {
+        System.out.println("This file will be added to other-test-branch by git");
+    }
+}
